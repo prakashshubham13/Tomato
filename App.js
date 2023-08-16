d1d88837a734f7470c21b6a6884a4ce149168145
prakashshubham13 <1806346@kiit.ac.in>
2023-08-17 03:21:31 +0530

    html and jd file created

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("div",{id:"body",key:"h"},[React.createElement("h2",{key:"n"},"hello"),React.createElement("h2",{key:"j"},"hello")]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
